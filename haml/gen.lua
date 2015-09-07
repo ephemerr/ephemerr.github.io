d0d618a01df1b6323ac58e5200acb9fc75d28242
ME <me@localhost.localdomain>
2015-09-07 14:27:31 +0300

    less conten, more haml

--- haml/gen.lua
+++ haml/gen.lua
@@ -1,19 +1,19 @@
 #!/usr/bin/lua
 
 local haml         = require "haml"
-local haml_options = {format = "html5"}
+local options      = {format = "html5"}
 local engine       = haml.new(options)
-
-
-local names = { "index", "theatre", "plays", "study", "contact" }
-
 local locals = {}
-
+local function elem( el_name )	
+	local el_haml = "../elem/" .. el_name .. ".haml"
+	local res = engine:render_file(el_haml, locals)	
+	return res
+end
+locals.elem = elem
+local names = { "index", "theatre", "plays", "study", "contact", "main" }
 for _, name in pairs(names) do 
-	locals.css = "/css/"..name..".less"
-	locals.content = io.open("../content/" .. name .. ".cont.html", "r"):read("*all")
-	local rendered = engine:render_file("frame.haml", locals)	
-	html_name = name == "index" and "../index.html" or "../html/"..name..".html"
+	local rendered = engine:render_file( "../haml/"..name..".haml", locals)
+	html_name = name == "main" and "../index.html" or "../html/"..name..".html"
 	io.open(html_name,"w+"):write(rendered);
 	print(name .. "...done")	
 end
