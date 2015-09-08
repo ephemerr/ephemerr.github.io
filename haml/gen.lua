cc0c1316e2f3e810104d07d03a930958f7b1cde6
ME <me@localhost.localdomain>
2015-09-08 12:47:06 +0300

    news plays generator

--- haml/gen.lua
+++ haml/gen.lua
@@ -4,12 +4,40 @@ local haml         = require "haml"
 local options      = {format = "html5"}
 local engine       = haml.new(options)
 local locals = {}
+
 local function elem( el_name )	
 	local el_haml = "../elem/" .. el_name .. ".haml"
 	local res = engine:render_file(el_haml, locals)	
 	return res
 end
 locals.elem = elem
+
+local function topnews()    
+    local max=3
+    local newsstr = io.open("../data/news.lua", "r"):read("*all")
+    loadstring(newsstr)()
+    local res = ""
+    for i=1,max do
+        locals.date, locals.newshead, locals.newsbody = news[i][1], news[i][2], news[i][3]
+        res = res .. engine:render_file( "../elem/news.haml", locals)
+    end
+    return res
+end
+locals.topnews = topnews
+
+local function getplaybill(max)
+    max = max or 1
+    local playstr = io.open("../data/playbill.lua", "r"):read("*all")
+    loadstring(playstr)()
+    local res = ""
+    for i=1,max do
+        setmetatable(locals,{__index=playbill[i]})
+        res = res .. engine:render_file( "../elem/playbill.haml", locals)
+    end
+    return res
+end
+locals.playbill = getplaybill   
+
 local names = { "index", "main",
                 "theatre", "plays", "study", "contact",
                 "hist", "rucov", "genre",
