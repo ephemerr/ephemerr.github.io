eaf48c4a20dcf7e4d814fd3e8fc8e9850177e99b
ME <me@localhost.localdomain>
2015-09-20 15:27:20 +0300

    articles + social

--- img/resize.sh
+++ img/resize.sh
@@ -2,14 +2,28 @@
 function files { 
     find  -name "*.jpg"  -printf "%p "
 }
-for file in `files`
-do
-    to_file="${file//.jpg/_max.jpg}"
-    echo "$file" "to" "$to_file"
-    convert "$file" -resize 1024x1024 -quality 75 "$to_file"
-done
+
+function max {
+    for file in `files`
+    do
+        to_file="${file//.jpg/_max.jpg}"
+        echo "$file" "to" "$to_file"
+        convert "$file" -resize 1024x1024 -quality 75 "$to_file"
+    done
+}
+
+function min {
+    for file in `files`
+    do
+        to_file="${file//.jpg/_min.jpg}"
+        echo "$file" "to" "$to_file"
+        convert "$file" -resize 320x320 -quality 75 "$to_file"
+    done
+}
 
 function out { 
-    find  -name "*_*_*.jpg"  -printf "%p "
+    find  -name "*_m*_m*.jpg"  -printf "%p "
 }
+
+min
 rm `out`
