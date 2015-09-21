f061e7452c2495013792ad952a5e1e094b795aac
azzel <a.zheleznov@gmail.com>
2015-09-21 16:22:55 +0000

    serpent

--- cgi/o.lua
+++ cgi/o.lua
@@ -2,7 +2,9 @@
 
 local orbit = require "orbit"
 local gen = require "gen"
-local ins = require "inspect"
+local serpent = require "serpent"
+local block = serpent.block
+local line = function(t) return  serpent.line(t, {comment=false}) end
 
 module("o", package.seeall, orbit.new)
 
@@ -21,14 +23,18 @@ function page(web, name)
         Set-Cookie: "username=aaa13; expires=Friday, 31-Dec-99 23:59:59 GMT; path=/; domain=my.dom;"
         ]]
         end
-    local inner_html = ins(web.GET) .. ins(web.input) 
+    local inner_html = block(web.GET) .. block(web.input) 
     return render_layout(inner_html)
 end
 
 function posthome(web, name)
-    local inner_html = ins(web.POST) 
-                        .. ins(web.input) 
-                        .. ins(web.vars)
+    local inner_html = line(web.POST) 
+                        .. block(web.vars)
+    local answer = io.open("../data/answer.lua","a+")
+    if answer then 
+        entry = {web.POST.name, web.POST.text}
+       answer:write(line(entry) .. ",\n")
+    end
     return render_layout(inner_html)
 end
 
