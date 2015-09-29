2ce0f405907d775785cb2edf3f35971b8f97d08d
azzel <a.zheleznov@gmail.com>
2015-09-29 12:52:27 +0000

    img size fix

--- js/imgzoom.js
+++ js/imgzoom.js
@@ -1,11 +1,8 @@
 function zoomin(img) {
-    img.style.display = "none";
-    replacer = img.parentNode.getElementsByClassName("maxi")[0];
-    replacer.style.display = "block";
+    maxi = img.parentNode.getElementsByClassName("maxi")[0];
+    maxi.style.display = "block";
 }
 
 function zoomout(img) {
     img.style.display = "none";
-    replacer = img.parentNode.getElementsByClassName("mini")[0];
-    replacer.style.display = "block";
 }
