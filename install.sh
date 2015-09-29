aa765a938f9db38cb8b8c4eb9d4b7a4f282e8c2f
azzel <a.zheleznov@gmail.com>
2015-09-29 13:17:54 +0000

    zoom-cursor

--- install.sh
+++ install.sh
@@ -5,20 +5,20 @@ cd css
 lessc "index.less" -o "index.css"
 lessc "playbill.less" -o "playbill.css"
 for file in *.less
-do  
+do
   lessc $file -o ${file//less/css}
 done
 sed -i s/\;body/\ body/g index.css # format fix
 cd -
 
 #
-cd cgi 
+cd cgi
 lua genall.lua
 cd -
 
 # put index.html evrywere
-IFS=$'\n'  
-DIRS=$(find * -type d) 
+IFS=$'\n'
+DIRS=$(find * -type d)
 for dir in $DIRS
 do
 	cp html/index.html $dir/
