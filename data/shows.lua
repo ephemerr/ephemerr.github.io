59c400d63df7c0612ab3e67b0210626d98a6cb7d
azzel <a.zheleznov@gmail.com>
2015-10-08 13:55:25 +0000

    fixed hist  fixed about

---
+++ data/shows.lua
@@ -0,0 +1,3 @@
+--[[ passed, month, day, wday, time, play, stage ]]--
+{ 1 ,  "Сентябрь" ,  "13" , "воскресенье" , "15:00" ,  "otcideti" , "malooht86" },
+{ 0 , "Октябрь"  ,  "11" , "воскресенье" , "15:00" ,  "karenina" , "malooht86" },
